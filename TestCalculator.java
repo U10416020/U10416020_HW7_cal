55dbfccbea2099ea1fbe43679b55f59a3227f50f
U10416020 <wkps19961015@gmail.com>
2016-03-24 17:52:53 +0800

    Create a class named TestCalculator
    Set a main method to display the calculator.

---
+++ TestCalculator.java
@@ -0,0 +1,9 @@
+//U10416020
+
+import javafx.application.Application;
+
+public class TestCalculator {
+	public static void main(String[] args){
+		Application.launch(Calculator.class, args);
+	}
+}
